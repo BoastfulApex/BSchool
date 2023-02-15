b5ed6e7c7f2041dd64fdeca5fe6dc09f3735e07e
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2023-02-16 01:25:27 +0500

    views added

--- school/views.py
+++ school/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from .serializers import *
+from rest_framework import viewsets
 
-# Create your views here.
+
+class AboutView(viewsets.ModelViewSet):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+
+
+class SliderView(viewsets.ModelViewSet):
+    queryset = Slider.objects.all()
+    serializer_class = SliderSerializer
+    
+
+class PartnerView(viewsets.ModelViewSet):
+    queryset = Partners.objects.all()
+    serializer_class = PartnerSerializer
+    
+
+class NewsView(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+    
+
+class DocumentView(viewsets.ModelViewSet):
+    queryset = Documents.objects.all()
+    serializer_class = DocumentSerializer
+    
+
+class AboutView(viewsets.ModelViewSet):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+    
+
+class LeadershipView(viewsets.ModelViewSet):
+    queryset = Leadership.objects.all()
+    serializer_class = LeadershipSerializer
+    
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = CourseCategory.objects.all()
+    serializer_class = CategorySerializer
+    
+
+class CourseView(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
