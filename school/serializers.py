b49ba36ea396ec422dab2ed448f1be0d2fa1dd77
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2023-02-16 01:21:27 +0500

    serializers added

---
+++ school/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import *
+
+class SliderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Slider
+        fields = '__all__'
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Partners
+        fields = '__all__'
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Documents
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseCategory
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class LeadershipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leadership
+        fields = '__all__'
+
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields = '__all__'
+
